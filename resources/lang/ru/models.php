85b4bef4fa720f6b669cada3081870bc9f9d7a53
Sergey Dyachok <sergey@sdyachok.com.ua>
2015-08-30 14:48:56 +0300

    Localization and style improvements

---
+++ resources/lang/ru/models.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'alias' => [
+        'source' => 'Исходный адрес',
+        'destination' => 'Адрес назначения',
+    ],
+
+    'common' => [
+        'active' => 'Активен?',
+        'domain_id' => 'Домен',
+        'created_at' => 'Создано',
+        'updated_at' => 'Обновлено',
+    ],
+
+    'domain' => [
+        'fqdn' => 'Домен',
+    ],
+
+    'mailbox' => [
+        'email' => 'Email',
+        'password' => 'Пароль',
+        'last_login' => 'Последний вход'
+    ],
+];
